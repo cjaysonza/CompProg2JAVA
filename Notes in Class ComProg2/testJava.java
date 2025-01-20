8e11844d815aafc905f5c87bbedbf2fb57fb7b0b
Christian Sonza <christianjaysonza@gmail.com>
2025-01-20 19:31:54 +0800

    input is in one line

--- Notes in Class ComProg2/testJava.java
+++ Notes in Class ComProg2/testJava.java
@@ -6,7 +6,7 @@ public class testJava {
 
         boolean exitProgram = false;
 
-        while (exitProgram != true) {
+        while (!exitProgram) {
             char userInput;
             System.out.println("What would you like to do?");
             System.out.println("[c] Use Calculator");
@@ -15,24 +15,23 @@ public class testJava {
             userInput = scanner.nextLine().toLowerCase().charAt(0);
 
             // USE THE CALCULATOR
-            if(userInput == 'c') {
+            // If, else if, else is used instead of switch is intentional
+            if (userInput == 'c') {
                 double val1, val2;
                 char operation;
 
                 System.out.println("\nThis is a Calculator");
                 System.out.println("Just type out in this format");
-                System.out.println("(y [ENTER] operation [ENTER] y [ENTER])");
+                System.out.println("([x] [SPACE] [Operation] [SPACE] [y]");
                 System.out.println("[+]Add [-]Subtract");
                 System.out.println("[*]Mulitply [/]Divide");
 
-                System.out.print("Value 1>:\t");
+                System.out.print("->:\t");
                 val1 = scanner.nextDouble();
-                scanner.nextLine();
-                System.out.print("Operation>:\t");
-                operation = scanner.nextLine().charAt(0);
-                System.out.print("Value 2>:\t");
+                operation = scanner.next().charAt(0);
                 val2 = scanner.nextDouble();
                 scanner.nextLine();
+
             // PROCESS THE OPERATION AND GIVES THE ANSWER
                 switch (operation) {
                     case '+':
@@ -56,10 +55,12 @@ public class testJava {
                     break;
                 }
             } else if (userInput == 'x') {
+                // Exits the program
                 exitProgram = true;
                 System.out.println("Exiting Program...");
                 System.out.println("Thank you for using my calculator");
             } else {
+                // Any wrong user input
                 System.out.println("Invalid input. Try Again\n");
             }
         }     
